6d61a08591ad92cc74738832c6d6b3de2e6729de
afikri <remotesoft.dev@gmail.com>
2026-09-02 15:01:48 +0700

    fix: hardcode CORS regex for Vercel preview deployments

--- frontend/backend_app/main.py
+++ frontend/backend_app/main.py
@@ -84,7 +84,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.CORS_ORIGINS,
-    allow_origin_regex=settings.CORS_ORIGIN_REGEX,
+    allow_origin_regex=r"https://.*-ict-4-d\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
